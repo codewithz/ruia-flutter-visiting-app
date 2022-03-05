1391c06b23699810e8bfb0b21cb9143897f14a85
Zartab Nakhwa <zartab@codewithz.com>
2022-03-05 12:54:59 +0530

    Added Email block and Center Aligned Vertically

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
      home:Scaffold(
        backgroundColor: Colors.deepPurple,
        body: SafeArea(
+         child:Center(
          child:Column(
+           mainAxisAlignment: MainAxisAlignment.center,
           children:<Widget>[
             CircleAvatar(
               radius: 50,
@@ -27,8 +29,43 @@ class MyApp extends StatelessWidget {
                 fontSize: 40,
                 fontWeight: FontWeight.bold
               )
+            ),
+            Text(
+              "CEO,  Asterix Solution",
+              style:TextStyle(
+                color: Colors.deepPurple.shade100,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5
+              )
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.deepPurple,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "zartab@codewithz.com",
+                    style:(
+                    TextStyle(
+                      color:Colors.deepPurple.shade300,
+                      fontSize: 20
+                    )
+                    )
+                  )
+                ],
+              )
             )
           ],
+         )
          )
        ),
      )
